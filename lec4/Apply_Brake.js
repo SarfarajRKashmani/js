9e5cbb6c1d3585449a1ee0982a13f31b83ddf106
Shadow <shadow@192.168.1.6>
2024-04-18 22:47:02 +0530

    comp

---
+++ lec4/Apply_Brake.js
@@ -0,0 +1,9 @@
+let distance=788
+let time=3
+let speed=distance/time
+if(speed>40){
+    console.log("Apply Brake");
+}
+else{
+    console.log("Keep Going");
+}
--- lec4/Compare_two_Numbers.js
+++ lec4/Compare_two_Numbers.js
@@ -1,18 +1,21 @@
 let num1=4
-let num2=8
+let num2=16
 if (num1>num2){
     console.log("true");
 }
 else{
     console.log("false");
 }
-let num3=5
-num1+=num3
-if (num1>num2){
+if (num1<num2){
     console.log("true");
 }
 else{
     console.log("false");
 }
-
+if (num1==num2){
+    console.log("true");
+}
+else{
+    console.log("false");
+}
 
