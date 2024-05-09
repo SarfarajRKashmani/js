cd0e0765f7637f77d58dc91e72fb49bf93b78af5
Sarfarajkashmani <sarfrajkashmani5@gmail.com>
2024-05-09 19:59:38 +0530

    comp

---
+++ lec7/lower_case.js
@@ -0,0 +1,16 @@
+// const getvalue=()=>{
+//     let str=document.getElementById("str1").value;
+//     div(str)
+// }
+// const div=(str1)=>{
+//     document.getElementById("result").append=toUppercase()
+// }
+const upper=(arr)=>{
+    for(let i=0;i<arr.length;i++)
+    {
+        console.log(arr[i].toUpperCase());
+    }
+}
+let arr="red and white";
+upper(arr);
+console.log(arr.toUpperCase());
---
+++ lec7/odd_position.js
@@ -0,0 +1,6 @@
+const str="redandwhite"
+for(let i=0;i<str.length;i++){
+    if(i%2!==0){
+        console.log(str[i]);
+    }
+}
