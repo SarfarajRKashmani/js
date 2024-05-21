9d52d6746131fb71f2441577b5a5d96f6375103f
Sarfarajkashmani <sarfrajkashmani5@gmail.com>
2024-05-21 17:20:28 +0530

    exam

--- dragon/dragon.js
+++ dragon/dragon.js
@@ -1,25 +1,42 @@
-let shop=[]
-const uimaker=()=>{
-    document.getElementById("cart").innerHTML=""
+const products=[]
+    const delete1=(index)=>{
+        products.splice(index,1)
+        uimaker();
+    }
+    const uimaker=()=>{
+        document.getElementById("cart").innerHTML=""
+        products.map((items,index)=>{
+        let img=document.createElement('img')
+        img.src=items.image1
+        let price=document.createElement('p')
+        price.innerHTML=items.price1
+        let tile=document.createElement('p')
+        tile.innerHTML=items.title1
+        let btn1=document.createElement('button')
+        btn1.innerHTML="BUY NOW"
+        btn1.addEventListener("click",()=>{
+            alert("Added To Cart Sucessfully")
+        })
+        let btn2=document.createElement('button')
+        btn2.innerHTML="Delete"
+        btn2.addEventListener("click",()=>delete1())
+        let div1=document.createElement('div')
+        div1.append(btn1,btn2)
 
-    for (let i=0;i<students.length;i++){
-        let p=document.createElement("p")
-        a.innerHTML=students[i].title
-        let c=document.createElement("p")
-        b.innerHTML=students[i].price
-        let d=document.createElement("img")
-        c.innerHTML=students[i].url
-        documents.getElementById("cart").append9(a,b,c)
+        let div2=document.createElement('div')
+        div2.append(img,price,tile,div1)
+        document.getElementById("cart").append(div2)})
     }
-}
-const handledata=(e)=>{
-    e.preventdefault();
-    let data={
-        title:document.getElementById("title").value,
-        price:document.getElementById("price").value,
-        url:document.getElementById("url").value
+
+const handeldata=(e)=>{
+    e.preventDefault();
+    let cart1 ={
+        image1:document.getElementById("image").value,
+        title1:document.getElementById("title").value,
+        price1:document.getElementById("price").value   
     }
-    students.push(data)
-    uimaker()
+        products.push(cart1)
+     uimaker()
 }
-document.getElementById("f1").addEventListener("submit",handledata)
+document.getElementById('data').addEventListener("submit",handeldata)
+
