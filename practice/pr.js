e0fe44c23e278209108180a9893f468fbc1cff64
Sarfarajkashmani <sarfrajkashmani5@gmail.com>
2024-05-29 18:16:25 +0530

    counter

--- practice/pr.js
+++ practice/pr.js
@@ -1,16 +1,9 @@
-let t1=document.getElementById("1");
-let submit =document.getElementById("sub")
-let todo1=document.querySelector(".todo1")
-
-
-const todo=()=>{
-    let n1=document.createElement("p");
-    n1.textContent=t1.value;
-   todo1.append(n1)
-   t1.textContent="";
-
-};
-
-todo1.addEventListener("click",)
-submit.addEventListener("click",todo());
-
+let inputelem = document.getElementById("inpu")
+let clickbtn = document.querySelector(".btn")
+let todoFirst = document.getElementById("todoFirst")
+const addtodo = () => {
+    todoFirst.textContent = inputelem.value;
+}
+clickbtn.addEventListener("click", () => {
+    addtodo();
+});
